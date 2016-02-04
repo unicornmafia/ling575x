81aa86c95a882e3812222cfaa7d1dacc2e9b366a
Thomas Marsh <pretend.intellectual@gmail.com>
2016-02-04 00:49:24 -0800

    added the determinations for SOV, SV, OV there is something in there for non-determinate too, but it's not done.    there are better accuracies now, though not as good as I expected.  Also, the code is getting big.  need to pull some stuff into modules.

--- src/profile.py
+++ src/profile.py
@@ -27,14 +27,45 @@ odin_corpus = glob.glob(os.path.join(odin_path, "*.xml"))
 # this is an annoying required conversion between WALS language ids and ISO language ids
 wals_dictionary = WalsLanguageCodes(os.path.join(wals_path, "wals-language"))
 
-# this is a simple list of languages and their gold-value.
-wals = WalsFeature(os.path.join(wals_path, "wals-dataset"), "81A")
+# this is a simple list of languages and their gold-value for feature.
+wals_svo = WalsFeature(os.path.join(wals_path, "wals-dataset"), "81A")
+wals_sv = WalsFeature(os.path.join(wals_path, "wals-dataset"), "82A")
+wals_ov = WalsFeature(os.path.join(wals_path, "wals-dataset"), "83A")
+wals_ndo = WalsFeature(os.path.join(wals_path, "wals-dataset"), "81B")
 
 # some globals
-feature_dictionary = {}
+svo_feature_dictionary = {}
+sv_feature_dictionary = {}
+ov_feature_dictionary = {}
+
 num_languages = len(odin_corpus)
 i = 0
 num_languages_determined = 0
+ndo_languages = []
+
+svo_possibilities = ["SOV", "SVO", "VSO", "VOS", "OVS", "OSV", "ndo"]  # ndo is "no dominant order"
+sv_possibilities = ["SV", "VS", "ndo"]  # ndo is "no dominant order"
+ov_possibilities = ["OV", "VO", "ndo"]  # ndo is "no dominant order"
+
+
+def print_order_confusion_matrix_for_feature(feature_dictionary, wals_gold_standard, possibilities, title):
+    # build and print a confusion matrix for svo
+    print("\nComparing results with gold standard from WALS for " + title + ":")
+    confusion_matrix = ConfusionMatrix(title, possibilities)
+    num_reported = 0
+    for code in feature_dictionary:
+        our_value = feature_dictionary[code]
+        try:
+            wals_code = wals_dictionary.iso_to_wals[code]
+            wals_value = wals_gold_standard.feature_dictionary[wals_code]
+            confusion_matrix.addLabel(wals_value, our_value)
+            num_reported += 1
+        except KeyError:
+            print("No matching SVO WALS data for language " + code)
+    print("Num Languages Compared: " + str(num_reported))
+    if num_reported > 0:
+        print(confusion_matrix.printMatrix())
+
 
 # START REALLY DOING STUFF HERE
 # loop through and determine our best guess
@@ -43,44 +74,42 @@ for language in odin_corpus:
     filename = os.path.basename(language)
     language_code = os.path.splitext(filename)[0]
     try:
+        # todo:  this is going to change.   we will fail later when we're doing confusion matrix
         # this is just a check to see if we get an error here.
         # we're going to error out if we can't look up this language/feature in WALS.
         wals_code = wals_dictionary.iso_to_wals[language_code]
-        wals_value = wals.feature_dictionary[wals_code]
+        wals_value = wals_svo.feature_dictionary[wals_code]
     except KeyError:  # it wasn't in the dictionary of languages which have reported stats for feature in WALS
         continue
+
+    # DEBUG
+    #language = "/opt/dropbox/15-16/575x/data/odin2.1/data/by-lang/xigt-enriched/eng.xml"
+    #language_code = "eng"
+
     xc = xigtxml.load(language, mode='full')
     svo_calc = SVO(xc, language_code)
     svo_calc.estimate_word_order_for_each_instance()
-    if svo_calc.instance_count > 0:
-        feature_dictionary[language_code] = svo_calc.best_guess
+    if svo_calc.total_instance_count > 0:
+        if svo_calc.svo_sorted_probs is not None:
+            svo_feature_dictionary[language_code] = svo_calc.svo_best_guess
+        if svo_calc.sv_sorted_probs is not None:
+            sv_feature_dictionary[language_code] = svo_calc.sv_best_guess
+        if svo_calc.ov_sorted_probs is not None:
+            ov_feature_dictionary[language_code] = svo_calc.ov_best_guess
+        if svo_calc.svo_best_guess == "ndo":
+            ndo_languages.append(language_code)
         svo_calc.print_language_name()
         svo_calc.print_order_estimates()
         num_languages_determined += 1
-
-    #if num_languages_determined >= 5:
+    # DEBUG
+    #if num_languages_determined >= 10:
     #    break
 
 print("Num Languages Determined: \n" + str(num_languages_determined))
 
-# build and print a confusion matrix
-print("\nComparing results with gold standard from WALS:")
-possibilities = ["SOV", "SVO", "VSO", "VOS", "OVS", "OSV", "ndo"]  # ndo is "no dominant order"
-confusion_matrix = ConfusionMatrix("Generated and WALS", possibilities)
-num_reported = 0
-for code in feature_dictionary:
-    our_value = feature_dictionary[code]
-    try:
-        wals_code = wals_dictionary.iso_to_wals[code]
-        wals_value = wals.feature_dictionary[wals_code]
-        confusion_matrix.addLabel(wals_value, our_value)
-        num_reported += 1
-    except KeyError:
-        print("No matching WALS data for language " + code)
+print_order_confusion_matrix_for_feature(svo_feature_dictionary, wals_svo, svo_possibilities, "SVO Data")
+print_order_confusion_matrix_for_feature(sv_feature_dictionary, wals_sv, sv_possibilities, "SV Data")
+print_order_confusion_matrix_for_feature(ov_feature_dictionary, wals_ov, ov_possibilities, "OV Data")
 
-print("Num Languages Compared: " + str(num_reported))
-
-if num_reported > 0:
-    print(confusion_matrix.printMatrix())
 
 
--- src/svo.py
+++ src/svo.py
@@ -28,53 +28,120 @@ class SVO:
     #
     def __init__(self, corpus, language_code, debug=False):
         self.SVO_order_probabilities = {"SOV": 0.0, "SVO": 0.0, "VSO": 0.0, "VOS": 0.0, "OVS": 0.0, "OSV": 0.0}
-        self.instance_count = 0
+        self.SVO_to_SV = {"SOV": "SV", "SVO": "SV", "VSO": "VS", "VOS": "VS", "OVS": "VS", "OSV": "SV"}
+        self.SVO_to_OV = {"SOV": "OV", "SVO": "VO", "VSO": "VO", "VOS": "VO", "OVS": "OV", "OSV": "OV"}
+        self.SV_order_probabilities = {"SV": 0.0, "VS": 0.0}
+        self.SV_possibilities = {"SV": ["SOV", "SVO", "OSV"], "VS": ["VSO", "VOS", "OVS"]}
+        self.OV_order_probabilities = {"OV": 0.0, "VO": 0.0}
+        self.OV_possibilities = {"OV": ["SOV", "OVS", "OSV"], "VO": ["SVO", "VSO", "VOS"]}
+        self.total_instance_count = 0
+        self.svo_instance_count = 0
+        self.sv_instance_count = 0
+        self.ov_instance_count = 0
         self.corpus = corpus
         self.language_code = language_code
         self.debug = debug
-        self.sorted_probs = []
-        self.best_guess = "unk"
-        self.ndo_threshold = 0.1
+        self.svo_sorted_probs = []
+        self.sv_sorted_probs = []
+        self.ov_sorted_probs = []
+        self.svo_best_guess = "unk"
+        self.ov_best_guess = "unk"
+        self.sv_best_guess = "unk"
+        self.ndo_threshold = 0.15
+        self.distribute_unknown_probabilities = False
+
     #
     # list_to_word_order()
     # parameter:
     #   list:  a list containing the WordPos objects in the correct sorted order
     # returns: a string for the most likely word order
     #
-    def list_to_word_order(self, list):
+    def list_to_word_order(self, word_list):
         #todo:  there may not be all three
         #todo:  if there are two, should we distribute them equally around the ones that they might match?
-        return list[0].grammatical_function+list[1].grammatical_function+list[2].grammatical_function
+        if len(word_list) == 1:
+            return word_list[0].grammatical_function  # this had better be "v"
+        elif len(word_list) == 2:
+            return word_list[0].grammatical_function+word_list[1].grammatical_function
+        else:  # len had better be 3
+            return word_list[0].grammatical_function+word_list[1].grammatical_function+word_list[2].grammatical_function
 
     #
     # estimate_word_order_for_instance(): estimates word order for a single instance
+    # note:  this only checks for non-passive constructions
     # parameters:
     #   igt - the igt
     #
     def estimate_word_order_for_instance(self, igt):
         function_list = []
-        words = igt["w"]
-        dparse = igt["w-ds"]
-        for pos in dparse:
+        try:
+            words = igt["w"]
+            dparse = igt["w-ds"]
+        except KeyError:
+            return
+
+        found_root = False
+        found_subj = False
+        found_obj = False
+        root_word = ""
+
+        if len(dparse) > 0 and dparse[0].text != "root":
+            return  # early exit if it doesn't have a root.  unusable
+        else:
+            found_root = True
+            pos = dparse[0]
+            root_word = pos.attributes["dep"]
             word = words[pos.attributes["dep"]]
             segmentation = word.segmentation
             word_token = ref.resolve(igt, segmentation)
-            #todo:  There may be more than one of some of these.
-            #todo:  do we need to check to see if the parent is the root?
-            if pos.text == "nsubj":
-                function_list.append(WordPos(word_token, "S", segmentation))
-            elif pos.text == "root":
-                function_list.append(WordPos(word_token, "V", segmentation))
-            elif pos.text == "dobj":
-                function_list.append(WordPos(word_token, "O", segmentation))
+            function_list.append(WordPos(word_token, "V", segmentation))
 
-        if len(function_list) == 3:
+        # loop looking for subject and object of that root
+        for pos in dparse:
+            if pos.text != "root":
+                word = words[pos.attributes["dep"]]
+                segmentation = word.segmentation
+                word_token = ref.resolve(igt, segmentation)
+                head_word = pos.attributes["head"]
+                if pos.text == "nsubj" and not found_subj and head_word == root_word:
+                    function_list.append(WordPos(word_token, "S", segmentation))
+                    found_subj = True
+                    if found_obj:
+                        break  # break out of the loop.   we have everything we need
+                elif pos.text == "dobj" and not found_obj and head_word == root_word:
+                    function_list.append(WordPos(word_token, "O", segmentation))
+                    found_obj = True
+                    if found_subj:
+                        break  # break out of the loop.   we have everything we need
+
+        # if we've found something, let's add some probabilities
+        if found_root and len(function_list) > 1:
+            self.total_instance_count += 1
             sorted_list = sorted(function_list)
-            self.instance_count += 1
             word_order = self.list_to_word_order(sorted_list)
-            self.SVO_order_probabilities[word_order] += 1.0
-            if self.debug:
-                self.debug_log("WORD-ORDER: " + word_order)
+            if found_subj and found_obj:
+                self.svo_instance_count += 1
+                self.sv_instance_count += 1
+                self.ov_instance_count += 1
+                self.SVO_order_probabilities[word_order] += 1.0
+                self.SV_order_probabilities[self.SVO_to_SV[word_order]] += 1.0
+                self.OV_order_probabilities[self.SVO_to_OV[word_order]] += 1.0
+                if self.debug:
+                    self.debug_log("WORD-ORDER: " + word_order)
+            elif found_subj:
+                self.sv_instance_count += 1
+                self.SV_order_probabilities[word_order] += 1.0
+                # if we don't know obj, but we know sv, let's distribute evenly
+                if not found_obj and self.distribute_unknown_probabilities:
+                    for order in self.SV_possibilities[word_order]:  # word_order is either "SV" or "VS"
+                        self.SVO_order_probabilities[order] += 1.0/len(self.SV_possibilities[word_order])
+            elif found_obj:
+                self.ov_instance_count += 1
+                self.OV_order_probabilities[word_order] += 1.0
+                # if we don't know subj, but we know sv, let's distribute evenly
+                if not found_subj and self.distribute_unknown_probabilities:
+                    for order in self.OV_possibilities[word_order]:  # word_order is either "OV" or "VO"
+                        self.SVO_order_probabilities[order] += 1.0/len(self.OV_possibilities[word_order])
 
     #
     # log to console if debugging is turned on
@@ -83,6 +150,24 @@ class SVO:
         if self.debug:
             print(string)
 
+    def generate_best_guess(self, probabilities):
+        best_guess = "unk"
+        sorted_probs = sorted(probabilities, key=probabilities.get, reverse=True)
+        if len(sorted_probs) == 0:
+            return ["unk", None]
+        elif len(sorted_probs) == 1:
+            if probabilities[sorted_probs[0]] == 0.0:
+                return ["unk", None]
+            best_guess = sorted_probs[0]
+        elif len(sorted_probs) > 1:  # see if the top one is over the threshold from the secondmost one
+            if probabilities[sorted_probs[0]] == 0.0:
+                return ["unk", None]
+            diff = probabilities[sorted_probs[0]]-probabilities[sorted_probs[1]]
+            if diff > self.ndo_threshold:
+                best_guess = sorted_probs[0]
+            else:
+                best_guess = "ndo"  # no dominate order because probabilities of first two are too similar
+        return [best_guess, sorted_probs]
 
     #
     # estimate_word_order_for_each_instance() - loops through all the igt instances and calculates word order.
@@ -92,44 +177,23 @@ class SVO:
     #
     def estimate_word_order_for_each_instance(self):
         for igt in self.corpus:
-            try:
-                # print the phrases
-                phrases = igt["p"]
-                # print the dependency parse of the original text
-                words = igt["w"]
-                dparse = igt["w-ds"]
-                first_time = True
-                for pos in dparse:
-                    # print the igt instance name the first time
-                    if first_time:
-                        first_time = False
-                        self.debug_log("\nIGT INSTANCE: " + igt.id)
-                        self.debug_log("TEXT: ")
-                        for phrase in phrases:
-                            text = igt["n"][phrase.content].text
-                            self.debug_log("  text: " + text)
-                        self.debug_log("DEPENDENCY PARSE: ")
+           self.estimate_word_order_for_instance(igt)
 
-                    word = words[pos.attributes["dep"]]
-                    segmentation = word.segmentation
-                    word_token = ref.resolve(igt, segmentation)
-                    self.debug_log("  " + word_token + ": " + pos.text)
-                # add word order estimate
-                if not first_time:
-                    self.estimate_word_order_for_instance(igt)
-            except:
-                # just move on for now.  this will happen a lot when data is not present
-                pass
-        if self.instance_count > 0:
-            self.sorted_probs = sorted(self.SVO_order_probabilities, key=self.SVO_order_probabilities.get, reverse=True)
-            if len(self.sorted_probs) == 1:
-                self.best_guess = self.sorted_probs[0]
-            elif len(self.sorted_probs) > 1:  # see if the top one is over the threshold from the secondmost one
-                diff = self.SVO_order_probabilities[self.sorted_probs[0]]-self.SVO_order_probabilities[self.sorted_probs[1]]
-                if diff > self.ndo_threshold:
-                    self.best_guess = self.sorted_probs[0]
-                else:
-                    self.best_guess = "ndo"  # no dominate order because probabilities of first two are too similar
+        if self.svo_instance_count > 0:
+            # todo: UGLY!  will fix
+            retval = self.generate_best_guess(self.SVO_order_probabilities)
+            self.svo_best_guess = retval[0]
+            self.svo_sorted_probs = retval[1]
+
+        if self.sv_instance_count > 0:
+            retval = self.generate_best_guess(self.SV_order_probabilities)
+            self.sv_best_guess = retval[0]
+            self.sv_sorted_probs = retval[1]
+
+        if self.ov_instance_count > 0:
+            retval = self.generate_best_guess(self.OV_order_probabilities)
+            self.ov_best_guess = retval[0]
+            self.ov_sorted_probs = retval[1]
 
     #
     # print_language_name() - print the name of the language as stored in first instance metadata
@@ -147,12 +211,13 @@ class SVO:
     # print_order_estimates() - prints the list of probabilities of the word orders
     #
     def print_order_estimates(self):
-        if self.instance_count > 0:
-            print("SOV ORDER PROBABILITIES:  " + str(self.instance_count) + " Instances")
-            for order in self.sorted_probs:
-                prob = self.SVO_order_probabilities[order]/float(self.instance_count)
+        if self.svo_instance_count > 0 and self.svo_sorted_probs is not None:
+            print("SOV ORDER PROBABILITIES:  " + str(self.svo_instance_count) + " Instances")
+            for order in self.svo_sorted_probs:
+                prob = self.SVO_order_probabilities[order]/float(self.svo_instance_count)
                 if prob > 0.0:
                     print(order + ": " + str(prob))
             print("")
         else:
             print("NO USABLE INSTANCES FOUND:  SOV order undeterminable from data\n")
+
